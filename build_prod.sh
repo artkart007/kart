fbbd4f76afe628742b7bfca715b16cac648e6ac5
artkart <artkart007@gmail.com>
2018-09-11 16:51:15 +0530

    fc

--- build_prod.sh
+++ build_prod.sh
@@ -6,7 +6,7 @@ kart_service_path="$root_path/kart-service"
 
 #build UI
 kart_ui_path="$root_path/kart-ui"
-cd $kart_ui_path && yarn build && cp -r build/* "$kart_service_path/src/main/resources/static"
+cd $kart_ui_path && yarn && yarn build && cp -r build/* "$kart_service_path/src/main/resources/static"
 
 #build WAR
 cd $kart_service_path && ./mvnw clean package
