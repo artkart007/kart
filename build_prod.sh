5440dfd253095c414bd68e8d0a9997cf997be3db
artkart <artkart007@gmail.com>
2018-09-11 17:32:34 +0530

    fc

--- build_prod.sh
+++ build_prod.sh
@@ -12,5 +12,4 @@ cd $kart_ui_path && yarn && yarn build && cp -r build/* "$kart_service_path/src/
 cd $kart_service_path && ./mvnw clean package
 
 #start app
-
-cd $kart_service_path && nohup java -jar target/kart-0.0.1-SNAPSHOT.jar &
+cd $kart_service_path && java -jar target/kart-0.0.1-SNAPSHOT.jar 
